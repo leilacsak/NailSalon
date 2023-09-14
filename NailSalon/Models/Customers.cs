abd010b19c06c80b3599bb18b04b5d3161df57f8
leilacsak <leilacsak@gmail.com>
2023-09-14 14:01:13 +0200

    Add project files.

---
+++ NailSalon/Models/Customers.cs
@@ -0,0 +1,12 @@
+﻿namespace NailSalon.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
